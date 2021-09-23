24cb79417ac26276c8a1893953627cb60fcc31e2
Martin Guillon <martin.guillon@akylas.fr>
2021-09-23 13:09:20 +0200

    fix: should fix setup on macos

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,21 +1,6 @@
 #!/bin/bash -e
 
-function abs_path()
-{
-  readlink="readlink -f"
-  if [[ "$(uname)" == "Darwin" ]]; then
-    if [[ ! "$(command -v greadlink)" ]]; then
-      echo "greadlink not found. Please install greadlink by \`brew install coreutils\`" >&2
-      exit 1
-    fi
-    readlink="greadlink -f"
-  fi
-
-  echo `$readlink $1`
-}
-
-
-CURR_DIR=$(dirname $(abs_path $0))
+CURR_DIR=$(dirname $(realpath $0))
 ROOT_DIR=$(dirname ${CURR_DIR})
 unset CURR_DIR
 
