03408a22f2910060c5bcc359a3ba0c0dc8a82cae
Martin Guillon <martin.guillon@akylas.fr>
2021-09-26 15:18:43 +0200

    working again

--- scripts/build.android.sh
+++ scripts/build.android.sh
@@ -17,8 +17,7 @@ fi
 
 # The order of CPU architectures in this array must be the same
 # as the order of NDK tools in the NDK_BUILD_TOOLS_ARR array
-ARCH_ARR=(arm64)
-# ARCH_ARR=(arm arm64 x86 x64)
+ARCH_ARR=(arm arm64 x86 x64)
 
 BUILD_DIR_PREFIX="outgn"
 
@@ -33,15 +32,13 @@ for CURRENT_ARCH in ${ARCH_ARR[@]}
 do
         if [[ $BUILD_TYPE == "debug" ]] ;then
                 gn gen $BUILD_DIR_PREFIX/$CURRENT_ARCH-$BUILD_TYPE --args="is_component_build=true v8_use_external_startup_data=true is_debug=true symbol_level=2 target_cpu=\"$CURRENT_ARCH\" v8_target_cpu=\"$CURRENT_ARCH\" v8_enable_i18n_support=false target_os=\"android\" v8_android_log_stdout=false"
-               # gn gen $BUILD_DIR_PREFIX/$SNAPSHOT_PREFIX$CURRENT_ARCH-$BUILD_TYPE --args="is_component_build=false v8_use_external_startup_data=true is_debug=true symbol_level=2 target_cpu=\"$CURRENT_ARCH\" v8_target_cpu=\"$CURRENT_ARCH\" v8_enable_i18n_support=false target_os=\"android\" v8_android_log_stdout=false"
         else
-                ARGS="is_clang=true enable_resource_allowlist_generation=false is_component_build=true v8_use_external_startup_data=false is_official_build=true use_thin_lto=false is_debug=false symbol_level=0 target_cpu=\"$CURRENT_ARCH\" v8_target_cpu=\"$CURRENT_ARCH\" v8_enable_i18n_support=false target_os=\"android\" v8_android_log_stdout=false"
+                ARGS="is_clang=true enable_resource_allowlist_generation=false is_component_build=true v8_enable_webassembly=false v8_use_external_startup_data=false is_official_build=true use_thin_lto=false is_debug=false symbol_level=0 target_cpu=\"$CURRENT_ARCH\" v8_target_cpu=\"$CURRENT_ARCH\" v8_enable_i18n_support=false target_os=\"android\" v8_android_log_stdout=false use_custom_libcxx=true"
                 if [[ $CURRENT_ARCH =~ 64$ ]] ;then
                         gn gen $BUILD_DIR_PREFIX/$CURRENT_ARCH-$BUILD_TYPE --args="$ARGS"
                 else
                         gn gen $BUILD_DIR_PREFIX/$CURRENT_ARCH-$BUILD_TYPE --args="$ARGS android32_ndk_api_level=$NDK_API_LEVEL android_ndk_major_version=$NDK_API_LEVEL android_sdk_platform_version=$NDK_API_LEVEL android_sdk_version=$NDK_API_LEVEL android64_ndk_api_level=$NDK_API_LEVEL"
                 fi
-               # gn gen $BUILD_DIR_PREFIX/$SNAPSHOT_PREFIX$CURRENT_ARCH-$BUILD_TYPE --args="enable_resource_allowlist_generation=false is_component_build=false v8_use_external_startup_data=true is_official_build=true use_thin_lto=false is_debug=false symbol_level=0 target_cpu=\"$CURRENT_ARCH\" v8_target_cpu=\"$CURRENT_ARCH\" v8_enable_i18n_support=false target_os=\"android\" v8_android_log_stdout=false"
 
         fi
 done
@@ -52,7 +49,7 @@ for CURRENT_ARCH in ${ARCH_ARR[@]}
 do
 
         # make fat build
-        V8_FOLDERS=(v8_compiler v8_base_without_compiler v8_libplatform v8_snapshot v8_libbase v8_bigint v8_cppgc_shared)
+        V8_FOLDERS=(v8_compiler v8_base_without_compiler v8_libplatform v8_snapshot v8_libbase v8_bigint torque_generated_initializers torque_generated_definitions)
 
         SECONDS=0
         ninja -C $BUILD_DIR_PREFIX/$CURRENT_ARCH-$BUILD_TYPE ${V8_FOLDERS[@]} inspector
@@ -71,14 +68,15 @@ do
         LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/${CURRENT_V8_FOLDER}/*.o"
         done
 
-        ZLIB_INPUT="$OUTFOLDER/obj/third_party/zlib/zlib/*.o ${OUTFOLDER}/obj/third_party/zlib/zlib/*.o ${OUTFOLDER}/obj/third_party/zlib/zlib_adler32_simd/*.o ${OUTFOLDER}/obj/third_party/zlib/google/compression_utils_portable/*.o ${OUTFOLDER}/obj/third_party/zlib/zlib_inflate_chunk_simd/*.o"
+        LAST_PARAM="${LAST_PARAM} $OUTFOLDER/obj/third_party/inspector_protocol/crdtp/*.o $OUTFOLDER/obj/third_party/inspector_protocol/crdtp_platform/*.o"
+        LAST_PARAM="${LAST_PARAM} $OUTFOLDER/obj/third_party/zlib/zlib/*.o ${OUTFOLDER}/obj/third_party/zlib/zlib/*.o ${OUTFOLDER}/obj/third_party/zlib/zlib_adler32_simd/*.o ${OUTFOLDER}/obj/third_party/zlib/google/compression_utils_portable/*.o ${OUTFOLDER}/obj/third_party/zlib/zlib_inflate_chunk_simd/*.o"
 
-        LAST_PARAM="${LAST_PARAM}  ${OUTFOLDER}/obj/third_party/android_ndk/cpu_features/*.o"
-        LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/cppgc_base/*.o"
+        LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/third_party/android_ndk/cpu_features/*.o"
+        LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/cppgc_base/*.o ${OUTFOLDER}/obj/v8_cppgc_shared/*.o"
         
         
         if [[ $CURRENT_ARCH = "arm" || $CURRENT_ARCH = "arm64" ]]; then
-                ZLIB_INPUT="${ZLIB_INPUT} ${OUTFOLDER}/obj/third_party/zlib/zlib_arm_crc32/*.o"
+                LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/third_party/zlib/zlib_arm_crc32/*.o"
         fi
 
         if [[ $CURRENT_ARCH = "x86" || $CURRENT_ARCH = "x64" ]]; then
@@ -87,55 +85,6 @@ do
 
         THIRD_PARTY_OUT=$BUILD_DIR_PREFIX/$CURRENT_ARCH-$BUILD_TYPE/obj/buildtools/third_party
         LAST_PARAM="${LAST_PARAM} $THIRD_PARTY_OUT/libc++/libc++/*.o $THIRD_PARTY_OUT/libc++abi/libc++abi/*.o"
-
-        $CURRENT_BUILD_TOOL/ar r $DIST_DIR/$CURRENT_ARCH-$BUILD_TYPE/libzip.a $ZLIB_INPUT
         
         $CURRENT_BUILD_TOOL/ar r $DIST_DIR/$CURRENT_ARCH-$BUILD_TYPE/libv8.a ${LAST_PARAM}
-
-        $CURRENT_BUILD_TOOL/ar r $DIST_DIR/$CURRENT_ARCH-$BUILD_TYPE/libinspector_protocol.a $OUTFOLDER/obj/third_party/inspector_protocol/crdtp/*.o $OUTFOLDER/obj/third_party/inspector_protocol/crdtp_platform/*.o
-
-        # echo "=================================="
-        # echo "=================================="
-        # echo "Copying snapshot binaries for $CURRENT_ARCH"
-        # echo "=================================="
-        # echo "=================================="
-        # DIST="./dist/snapshots/$CURRENT_ARCH-$BUILD_TYPE/"
-        # mkdir -p $DIST
-
-        # SOURCE_DIR=
-        # if [[ $CURRENT_ARCH == "arm64" ]] ;then
-        #         SOURCE_DIR=$BUILD_DIR_PREFIX/$SNAPSHOT_PREFIX$CURRENT_ARCH-$BUILD_TYPE/clang_x64_v8_$CURRENT_ARCH
-        # elif [[ $CURRENT_ARCH == "arm" ]] ;then
-        #         SOURCE_DIR=$BUILD_DIR_PREFIX/$SNAPSHOT_PREFIX$CURRENT_ARCH-$BUILD_TYPE/clang_x86_v8_$CURRENT_ARCH
-        # elif [[ $CURRENT_ARCH == "x86" ]] ;then
-        #         SOURCE_DIR=$BUILD_DIR_PREFIX/$SNAPSHOT_PREFIX$CURRENT_ARCH-$BUILD_TYPE/clang_x86
-        # elif [[ $CURRENT_ARCH == "x64" ]] ;then
-        #         SOURCE_DIR=$BUILD_DIR_PREFIX/$SNAPSHOT_PREFIX$CURRENT_ARCH-$BUILD_TYPE/clang_x64
-        # fi
-
-        # cp -r $SOURCE_DIR/mksnapshot $DIST
-
-        # echo "=================================="
-        # echo "=================================="
-        # echo "Preparing snapshot headers for $CURRENT_ARCH"
-        # echo "=================================="
-        # echo "=================================="
-
-        # INCLUDE="$(pwd)/dist/$CURRENT_ARCH-$BUILD_TYPE/include"
-        # mkdir -p $INCLUDE
-
-        # SOURCE_DIR=
-        # if [[ $CURRENT_ARCH == "arm64" ]] ;then
-        #         SOURCE_DIR=$BUILD_DIR_PREFIX/$CURRENT_ARCH-$BUILD_TYPE/clang_x64_v8_$CURRENT_ARCH
-        # elif [[ $CURRENT_ARCH == "arm" ]] ;then
-        #         SOURCE_DIR=$BUILD_DIR_PREFIX/$CURRENT_ARCH-$BUILD_TYPE/clang_x86_v8_$CURRENT_ARCH
-        # elif [[ $CURRENT_ARCH == "x86" ]] ;then
-        #         SOURCE_DIR=$BUILD_DIR_PREFIX/$CURRENT_ARCH-$BUILD_TYPE/clang_x86
-        # elif [[ $CURRENT_ARCH == "x64" ]] ;then
-        #         SOURCE_DIR=$BUILD_DIR_PREFIX/$CURRENT_ARCH-$BUILD_TYPE/clang_x64
-        # fi
-
-        # pushd $SOURCE_DIR/..
-        # xxd -i snapshot_blob.bin > $INCLUDE/snapshot_blob.h
-        # popd
 done
