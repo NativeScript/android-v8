74e8ec1a302a950151624cd7aca6ae9d722a1de9
Martin Guillon <martin.guillon@akylas.fr>
2021-12-16 11:21:14 +0100

    fix correctly use the right ar to compile + package all inspector stuff
    # Conflicts:
    #	scripts/build.android.sh

--- scripts/build.android.sh
+++ scripts/build.android.sh
@@ -13,16 +13,17 @@ done
 shift $(expr ${OPTIND} - 1)
 ## prepare configuration
 
+
 if [ "$(uname)" == "Darwin" ]; then
-        NDK_BUILD_TOOLS_ARR=($ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/arm-linux-androideabi/bin \
-                $ANDROID_NDK/toolchains/aarch64-linux-android-4.9/prebuilt/darwin-x86_64/aarch64-linux-android/bin \
-                $ANDROID_NDK/toolchains/x86-4.9/prebuilt/darwin-x86_64/i686-linux-android/bin \
-                $ANDROID_NDK/toolchains/x86_64-4.9/prebuilt/darwin-x86_64/x86_64-linux-android/bin)
+        NDK_BUILD_TOOLS_ARR=([arm]=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/arm-linux-androideabi/bin \
+                     [arm64]=$ANDROID_NDK/toolchains/aarch64-linux-android-4.9/prebuilt/darwin-x86_64/aarch64-linux-android/bin \
+                     [x86]=$ANDROID_NDK/toolchains/x86-4.9/prebuilt/darwin-x86_64/i686-linux-android/bin \
+                     [x64]=$ANDROID_NDK/toolchains/x86_64-4.9/prebuilt/darwin-x86_64/x86_64-linux-android/bin)
 else
-        NDK_BUILD_TOOLS_ARR=($ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/arm-linux-androideabi/bin \
-                $ANDROID_NDK/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/aarch64-linux-android/bin \
-                $ANDROID_NDK/toolchains/x86-4.9/prebuilt/linux-x86_64/i686-linux-android/bin \
-                $ANDROID_NDK/toolchains/x86_64-4.9/prebuilt/linux-x86_64/x86_64-linux-android/bin)
+        NDK_BUILD_TOOLS_ARR=([arm]=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/arm-linux-androideabi/bin \
+                     [arm64]=$ANDROID_NDK/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/aarch64-linux-android/bin \
+                     [x86]=$ANDROID_NDK/toolchains/x86-4.9/prebuilt/linux-x86_64/i686-linux-android/bin \
+                     [x64]=$ANDROID_NDK/toolchains/x86_64-4.9/prebuilt/linux-x86_64/x86_64-linux-android/bin)
 fi
 
 # The order of CPU architectures in this array must be the same
@@ -46,14 +47,13 @@ do
                 if [[ $CURRENT_ARCH =~ 64$ ]] ;then
                         gn gen $BUILD_DIR_PREFIX/$CURRENT_ARCH-$BUILD_TYPE --args="$ARGS"
                 else
-                        gn gen $BUILD_DIR_PREFIX/$CURRENT_ARCH-$BUILD_TYPE --args="$ARGS android32_ndk_api_level=$NDK_API_LEVEL android_ndk_major_version=$NDK_MAJOR_VERSION android_sdk_platform_version=$ANDROID_SDK_PLATFORM_VERSION android_sdk_build_tools_version=\"$ANDROID_SDK_BUILD_TOOLS_VERSION\"  android_sdk_version=$ANDROID_SDK_PLATFORM_VERSION android64_ndk_api_level=21"
+                        gn gen $BUILD_DIR_PREFIX/$CURRENT_ARCH-$BUILD_TYPE --args="$ARGS android32_ndk_api_level=$NDK_API_LEVEL android_ndk_major_version=$NDK_MAJOR_VERSION android_sdk_platform_version=$ANDROID_SDK_PLATFORM_VERSION android_sdk_build_tools_version=\"$ANDROID_SDK_BUILD_TOOLS_VERSION\"  android_sdk_version=$ANDROID_SDK_PLATFORM_VERSION android64_ndk_api_level=$ANDROID_SDK_PLATFORM_VERSION"
                 fi
 
         fi
 done
 
 # compile project
-COUNT=0
 for CURRENT_ARCH in ${ARCH_ARR[@]}
 do
 
@@ -67,29 +67,29 @@ do
 
         echo "build finished in $SECONDS seconds"
 
-        mkdir -p $DIST_DIR/$CURRENT_ARCH-$BUILD_TYPE
 
         OUTFOLDER=${BUILD_DIR_PREFIX}/${CURRENT_ARCH}-${BUILD_TYPE}
-        CURRENT_BUILD_TOOL=${NDK_BUILD_TOOLS_ARR[$COUNT]}
-        COUNT=$COUNT+1
+        CURRENT_BUILD_TOOL=${NDK_BUILD_TOOLS_ARR[$CURRENT_ARCH]}
         V8_FOLDERS_LEN=${#V8_FOLDERS[@]}
         LAST_PARAM=""
         CURRENT_DIST_DIR="$DIST_DIR/$CURRENT_ARCH-$BUILD_TYPE"
+        echo "CURRENT_ARCH $CURRENT_ARCH"
+        echo "CURRENT_BUILD_TOOL $CURRENT_BUILD_TOOL"
+
+        mkdir -p $CURRENT_DIST_DIR
+
         for CURRENT_V8_FOLDER in ${V8_FOLDERS[@]}
         do
         LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/${CURRENT_V8_FOLDER}/*.o"
         done
 
-        $CURRENT_BUILD_TOOL/ar r $OUTFOLDER/obj/third_party/inspector_protocol/libinspector_protocol.a $OUTFOLDER/obj/third_party/inspector_protocol/crdtp/*.o $OUTFOLDER/obj/third_party/inspector_protocol/crdtp_platform/*.o
-        cp "$OUTFOLDER/obj/third_party/inspector_protocol/libinspector_protocol.a" "$CURRENT_DIST_DIR/libinspector_protocol.a"
+        $CURRENT_BUILD_TOOL/ar r $CURRENT_DIST_DIR/libinspector_protocol.a $OUTFOLDER/obj/third_party/inspector_protocol/crdtp/*.o $OUTFOLDER/obj/third_party/inspector_protocol/crdtp_platform/*.o
+        $CURRENT_BUILD_TOOL/ranlib $CURRENT_DIST_DIR/libinspector_protocol.a
 
-        ZLIB_INPUT="${OUTFOLDER}/obj/third_party/zlib/zlib/*.o ${OUTFOLDER}/obj/third_party/zlib/zlib_adler32_simd/*.o ${OUTFOLDER}/obj/third_party/zlib/google/compression_utils_portable/*.o ${OUTFOLDER}/obj/third_party/zlib/zlib_inflate_chunk_simd/*.o"
-	ar r $OUTFOLDER/obj/third_party/zlib/libzip.a $ZLIB_INPUT
-    	cp "$OUTFOLDER/obj/third_party/zlib/libzip.a" "$CURRENT_DIST_DIR/libzip.a"
+        LAST_PARAM="${LAST_PARAM} $OUTFOLDER/obj/third_party/zlib/zlib/*.o ${OUTFOLDER}/obj/third_party/zlib/zlib/*.o ${OUTFOLDER}/obj/third_party/zlib/zlib_adler32_simd/*.o ${OUTFOLDER}/obj/third_party/zlib/google/compression_utils_portable/*.o ${OUTFOLDER}/obj/third_party/zlib/zlib_inflate_chunk_simd/*.o"
 
         LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/third_party/android_ndk/cpu_features/*.o"
         LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/cppgc_base/*.o ${OUTFOLDER}/obj/v8_cppgc_shared/*.o"
-        
         
         if [[ $CURRENT_ARCH = "arm" || $CURRENT_ARCH = "arm64" ]]; then
                 LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/third_party/zlib/zlib_arm_crc32/*.o"
@@ -101,7 +101,6 @@ do
 
         THIRD_PARTY_OUT=$BUILD_DIR_PREFIX/$CURRENT_ARCH-$BUILD_TYPE/obj/buildtools/third_party
         LAST_PARAM="${LAST_PARAM} $THIRD_PARTY_OUT/libc++/libc++/*.o $THIRD_PARTY_OUT/libc++abi/libc++abi/*.o"
-
         
         $CURRENT_BUILD_TOOL/ar r $CURRENT_DIST_DIR/libv8.a ${LAST_PARAM}
         
@@ -111,20 +110,15 @@ do
         rsync -r --exclude '.git' --exclude '.git' "$V8_DIR/third_party/android_ndk/sources/cxx-stl/llvm-libc++/include/" "$CURRENT_DIST_DIR/include/libc++/"
         
         # generated files
-        rsync -r --exclude '.git' --exclude '.cache' --exclude 'snapshot.cc' --exclude 'embedded.S' "${OUTFOLDER}/gen/" "$CURRENT_DIST_DIR/generated/"
+        rsync -r --exclude '.git' --exclude '*.cache' --exclude 'snapshot.cc' --exclude 'embedded.S' "${OUTFOLDER}/gen/" "$CURRENT_DIST_DIR/generated/"
         
         mkdir -p "$CURRENT_DIST_DIR/v8_inspector"
-        cp -r "$CURRENT_DIST_DIR/generated/builtins-generated" "$CURRENT_DIST_DIR/v8_inspector"
         INSPECTOR_LIST=($(grep "\"+.*\""  $V8_DIR/src/inspector/DEPS | sed 's/[+,"]//g'))
         # remove last one ../../third_party/inspector_protocol/crdtp
         unset INSPECTOR_LIST[-1]
 	INSPECTOR_LIST+=("base/trace_event")
-
-	for INSPECTOR_FILE in ${INSPECTOR_LIST[@]}
-	do
-		rsync -aR --exclude '.git' --exclude '.cache' "$INSPECTOR_FILE" "$CURRENT_DIST_DIR/v8_inspector/"
-	done
-
-
-        
+	INSPECTOR_LIST+=("src/base")
+	INSPECTOR_LIST+=("src/common")
+        rsync -auR --exclude '.git' --exclude '*.cache' --exclude 'DEPS' --exclude 'DIR_METADATA'  --exclude 'OWNERS' --exclude 'BUILD.gn' "${INSPECTOR_LIST[@]}" "$CURRENT_DIST_DIR/v8_inspector/"
+        rsync -ur --exclude '.git' --exclude '*.cache' --exclude 'DEPS' --exclude 'DIR_METADATA'  --exclude 'OWNERS' --exclude 'BUILD.gn' "$CURRENT_DIST_DIR/generated/src/" "$CURRENT_DIST_DIR/v8_inspector/src/"
 done
