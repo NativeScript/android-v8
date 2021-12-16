f3507eb6a77c185a82e7edd36d0fc8bee5817843
Osei Fortune <fortune.osei@yahoo.com>
2021-12-16 18:58:52 -0400

    fix: build symbol

--- scripts/apply_patch.sh
+++ scripts/apply_patch.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -e
 
 pushd v8
+pushd build
+git apply --cached ../../patches/android/build.patch
+git checkout -- .
+popd
 git apply --cached ../patches/android/main.patch
 git checkout -- .
 popd
